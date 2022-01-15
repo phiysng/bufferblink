ff3c207a394bbeb22523545637b5d472b578eec1
Wu Yuanshou <wuyuanshou@protonmail.com>
2022-01-15 14:19:39 +0800

    add sample cmake project.

---
+++ bufferflip/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include"Lemon.pb.h"
+#include "Rocket.pb.h"
+
+using io::github::phiysng::Person;
+using io::github::phiysng::Rocket;
+int main(){
+    Person p;
+    p.set_id(100);
+    p.set_email("exmaple@gmail.com");
+    p.set_name("exmaple");
+     
+    p.PrintDebugString();
+
+    Rocket rocket;
+
+    rocket.mutable_p()->set_id(111);
+    rocket.PrintDebugString();
+}
