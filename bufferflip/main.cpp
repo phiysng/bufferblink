9810d03ded7a9f7f2039b838ea65a0b976a8dab2
Wu Yuanshou <wuyuanshou@protonmail.com>
2022-01-16 16:23:39 +0800

    move proto file to subproject.

--- bufferflip/main.cpp
+++ bufferflip/main.cpp
@@ -4,16 +4,16 @@
 
 using io::github::phiysng::Person;
 using io::github::phiysng::Rocket;
-int main(){
-    Person p;
-    p.set_id(100);
-    p.set_email("exmaple@gmail.com");
-    p.set_name("exmaple");
-     
-    p.PrintDebugString();
+int main() {
+	Person p;
+	p.set_id(100);
+	p.set_email("exmaple@gmail.com");
+	p.set_name("exmaple");
 
-    Rocket rocket;
+	p.PrintDebugString();
 
-    rocket.mutable_p()->set_id(111);
-    rocket.PrintDebugString();
+	Rocket rocket;
+
+	rocket.mutable_p()->set_id(111);
+	rocket.PrintDebugString();
 }
--- bufferflop/main.cpp
+++ bufferflop/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
-#include "Sample.pb.h"
+#include "Flop.pb.h"
 
 using namespace std;
 
 int main() {
 
-    HelloMessage message;
-    message.set_name("100");
-    message.set_index(10);
+	HelloMessage message;
+	message.set_name("100");
+	message.set_index(10);
 
-    auto bytes = message.SerializeAsString();
+	auto bytes = message.SerializeAsString();
 
-    cout << message.DebugString() << endl;
+	cout << message.DebugString() << endl;
 
-    return 0;
+	return 0;
 }
