4eb5ebd5827d2261fea0354cf29dfbb000686cf4
Wu Yuanshou <wuyuanshou@protonmail.com>
2022-02-01 20:03:52 +0800

    add test project.

---
+++ buffertest/main.cpp
@@ -0,0 +1,17 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <doctest/doctest.h>
+#include "Flop.pb.h"
+int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+
+TEST_CASE("testing the factorial function")
+{
+    CHECK(factorial(1) == 1);
+    CHECK(factorial(2) == 2);
+    CHECK(factorial(3) == 6);
+    CHECK(factorial(10) == 3628800);
+    
+    HelloMessage hello;
+    hello.set_name("Name");
+    hello.PrintDebugString();
+}
